5400473bcbcec87ade9bf6d4779436b1ac502bea
zjgemi <liuxin_zijian@163.com>
2023-02-10 09:35:19 +0800

    add node.name and node.id to local variables
    Signed-off-by: zjgemi <liuxin_zijian@163.com>

--- workflow/controller/operator.go
+++ workflow/controller/operator.go
@@ -1729,6 +1729,10 @@ func (woc *wfOperationCtx) executeTemplate(ctx context.Context, nodeName string,
 	if orgTmpl.IsWorkflowStep() {
 		localParams["steps.name"] = orgTmpl.GetName()
 	}
+	localParams["node.name"] = nodeName
+	if node != nil {
+		localParams["node.id"] = node.ID
+	}
 	// Merge Template defaults to template
 	err = woc.mergedTemplateDefaultsInto(resolvedTmpl)
 	if err != nil {
